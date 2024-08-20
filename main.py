704b9342471e8d56879c8757a680ed2eef5b816f
developer-aaronlee <110876391+developer-aaronlee@users.noreply.github.com>
2024-08-20 15:44:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ segment_track = "https://api.segment.io/v1/track"
 
 qa_headers = {
     "Content-Type": "application/json",
-    "Authorization": "Basic dnJPdHdtWlU1Q25LN0RsQVFoQ1N1RzdjeFhSU3dZQmM6"
+    "Authorization": "Basic api_key"
 }
 
 prod_headers = {
     "Content-Type": "application/json",
-    "Authorization": "Basic aktFVHNnQzBJWlg4SExyRzl4VFNXYVQ1dzZhcWZTb3U6"
+    "Authorization": "Basic api_key"
 }
 
 
